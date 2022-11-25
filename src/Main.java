b05673e71b54c6e13d2056e120e3f552fd0f4cad
Rarepug1st <99208797+Rarepug1st@users.noreply.github.com>
2022-11-25 14:39:34 -0500

    Updated

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,26 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
+		
+		System.out.print("what is your number?");
+		double num = in.nextDouble();
+		Double Num = num-1;
+		System.out.print(num + "- 1 is " + Num);
+		
+		Double NUMe = Num*3;
+		System.out.println(Num + " x 3 is " + NUMe);
+	
+	Double NUm = Num+12;
+	
+	System.out.print(NUMe + " + 12 is " + NUm);
+	
+	Double NUM = NUm/3;
+	
+	System.out.println(NUm + " / 3 is " + NUM);
+	
+	
+	
+	
 	
 	}
 
